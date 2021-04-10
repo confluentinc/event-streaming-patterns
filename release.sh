e8280c7e0da99f8aa4b1b0409e03168a85f46b44
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-04-10 14:52:45 -0500

    update release script

--- release.sh
+++ release.sh
@@ -3,5 +3,6 @@
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "releasing branch $CURRENT_BRANCH to GH Pages"
 mkdocs gh-deploy
-git commit -am "Deploy $CURRENT_BRANCH to site"
+git add site/.
+git commit -m "Deploy $CURRENT_BRANCH to site"
 git push origin main
